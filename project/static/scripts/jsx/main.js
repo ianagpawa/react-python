727843691dbfe6bc0fd00f6961d66cdd0da5b848
Ian Agpawa <agpawaji@gmail.com>
2017-12-27 00:55:31 -0500

    refactor: Start refactor of search component

--- project/static/scripts/jsx/main.js
+++ project/static/scripts/jsx/main.js
@@ -1,43 +1,7 @@
-var DynamicSearch = React.createClass({
-
-  // sets initial state
-  getInitialState: function(){
-    return { searchString: '' };
-  },
-
-  // sets state, triggers render method
-  handleChange: function(event){
-    // grab value form input box
-    this.setState({searchString:event.target.value});
-    console.log("scope updated!");
-  },
-
-  render: function() {
-
-    var countries = this.props.items;
-    var searchString = this.state.searchString.trim().toLowerCase();
-
-    // filter countries list by value from input box
-    if(searchString.length > 0){
-      countries = countries.filter(function(country){
-        return country.name.toLowerCase().match( searchString );
-      });
-    }
-
-    return (
-      <div>
-        <input type="text" value={this.state.searchString} onChange={this.handleChange} placeholder="Search!" />
-        <ul>
-          { countries.map(function(country){ return <li>{country.name} </li> }) }
-        </ul>
-      </div>
-    )
-  }
-
-});
+import DynamicSearch from './DynamicSearch'
 
 // list of countries, defined with JavaScript object literals
-var countries = [
+const countries = [
   {"name": "Sweden"}, {"name": "China"}, {"name": "Peru"}, {"name": "Czech Republic"},
   {"name": "Bolivia"}, {"name": "Latvia"}, {"name": "Samoa"}, {"name": "Armenia"},
   {"name": "Greenland"}, {"name": "Cuba"}, {"name": "Western Sahara"}, {"name": "Ethiopia"},
